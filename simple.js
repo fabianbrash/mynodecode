113e9d4e4b8d16ae33d0f857f24231d490dc28d5
Fabian Brash <fabianbrash@gmail.com>
2017-05-25 15:55:10 -0400

    Initial Commit
    simple app connecting to and returning all values in a rethinkdb

---
+++ simple.js
@@ -0,0 +1,24 @@
+
+
+var r = require('rethinkdb');
+
+var connection = null;
+
+r.connect( {host: 'localhost', port: 28015}, function(err,conn) {
+  if(err) throw err;
+
+  connection = conn;
+
+
+r.db('NASA').table('peopleinspace').run(connection, function(err, cursor) {
+
+
+  cursor.toArray(function(err, result) {
+    if(err) throw err;
+
+    console.log(JSON.stringify(result, null, 2));
+  });
+
+ });
+
+});
